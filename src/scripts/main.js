065762cdab626301171a92686ce6e13d1b1b63c3
Alex Zhang <alex.zhang@berkeley.edu>
2014-08-16 22:36:13 -0700

    change ids

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -29,6 +29,19 @@ bespoke.from('article', [
 require('prism');
 
 
-$("#submit").click(function() {
-  console.log("test")
-})
+jQuery(function() {
+ // Schedule meeting modal window                        
+    jQuery(document).on('click', '#submit', function(ev) {
+    var url = $('#uq_url').val();
+     website_name = $('#nq_name').val();
+     ownership = $('#ow_name').val();
+     disclose_company= $('#dwc').val();
+     disclose_affiliates = $('#dwa').val();
+     disclose_third = $('#dwp').val();
+     litigate_city = $('#l_city').val();
+     litigate_state  = $('#l_state').val();
+     litigate_law_state  = $('#ll_state').val();
+   console.log(url + website_name+ ownership+ disclose_company+ disclose_company, litigate_city, litigate_state, litigate_law_state);
+ });
+
+});
